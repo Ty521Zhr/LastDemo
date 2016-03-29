76cc99ccd56dd23baffa3ed018b060013651841f
佟野 <1215353560@qq.com>
2016-03-29 10:26:40 +0800

    很牛逼后边那

--- app/src/main/java/com/lanou/lastdemo/MainActivity.java
+++ app/src/main/java/com/lanou/lastdemo/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//范德萨发生的发生 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
