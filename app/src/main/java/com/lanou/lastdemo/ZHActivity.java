0391be07561d4deb12f5ddc1739aee1526d55d12
ZHDelete <1414524461@qq.com>
2016-03-29 10:25:21 +0800

    赵一鸣 commit 1

---
+++ app/src/main/java/com/lanou/lastdemo/ZHActivity.java
@@ -0,0 +1,15 @@
+package com.lanou.lastdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZHDelete on 16/3/29.
+ */
+public class ZHActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //zh 功能完成
+    }
+}
